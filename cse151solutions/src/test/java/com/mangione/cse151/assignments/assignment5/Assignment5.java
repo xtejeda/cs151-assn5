b765ed1c82f9a837a48296ef9227aa34e7e79104
Xavier <tejedax12@gmail.com>
2016-06-03 00:15:46 -0700

    Revert "hotpatch"
    This reverts commit a9ff2b3898a1cec94452bdab80b352d53c513d6b.

--- cse151solutions/src/test/java/com/mangione/cse151/assignments/assignment5/Assignment5.java
+++ cse151solutions/src/test/java/com/mangione/cse151/assignments/assignment5/Assignment5.java
@@ -3,8 +3,6 @@ package com.mangione.cse151.assignments.assignment5;
 import com.mangione.cse151.observationproviders.CsvObservationProvider;
 import com.mangione.cse151.observations.Observation;
 import com.mangione.cse151.observations.ObservationFactory;
-import com.mangione.cse151.observationproviders.SampledObservationProvider;
-import org.apache.commons.math3.random.MersenneTwister;
 
 import java.io.File;
 
@@ -13,41 +11,38 @@ import java.io.File;
  */
 public class Assignment5
 {
-    public static final int SEED = 104395301;
-
     public static void main(String[] args) throws Exception
     {
         int spamTotal = 0;
         int hamTotal = 0;
 
-        double[] spamValues = new double[9275];
-        double[] hamValues = new double[9275];
+        int[] spamValues = new int[9275];
+        int[] hamValues = new int[9275];
 
-        double[] spamProb = new double[9275];
-        double[] hamProb = new double[9275];
+        int[] spamProb = new int[9275];
+        int[] hamProb = new int[9275];
 
-        double[][] trainingArray = new double[2798][9275];
+        int[][] trainingArray = new double[2798][9275];
 
-        final MersenneTwister randomForSampling = new MersenneTwister(SEED);
+        final RandomGenerator randomForSampling = new MersenneTwister(SEED);
         File file = new File(Assignment5.class.getClassLoader()
                 .getResource("com/mangione/cse151/assignment5/SpamDataPruned.csv").toURI());
         CsvObservationProvider<Observation> csvObsProvider = new CsvObservationProvider<>(file, new ObservationFactory());
 
-        SampledObservationProvider trainingSet = new SampledObservationProvider(.10, csvObsProvider, randomForSampling, false);
+        SampledObservationProvider trainingSet = new SampledObservationProvider(.90, csvObsProvider, randomForSampling, false);
 
         int k = 0;
-        while(trainingSet.hasNext() && k < 2798)
+        while(trainingSet.hasNext && i < 2798)
         {
-            System.out.println(k);
             trainingArray[k] = trainingSet.next().getFeatures();
             k++;
         }
 
-        for(int i = 0; i < 2518; i++)
+        for(int i = 0; i < 2798; i++)
         {
             for(int j = 0; j < 9275; j++)
             {
-                if(trainingArray[i][9274] == 1)
+                if(trainingArray[i][9274] = 1)
                 {
                     spamValues[j] = spamValues[j] + trainingArray[i][j];
                     spamTotal++;
@@ -65,7 +60,7 @@ public class Assignment5
             spamProb[a] = ( spamValues[a] + 1 ) / ( spamTotal + 9275 );
         }
 
-        for(int b = 0; b < 9275; b++)
+        for(int b = 0; b < 9275; a++)
         {
             hamProb[b] = ( hamValues[b] + 1 ) / ( hamTotal + 9275 );
         }
